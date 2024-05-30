94e8764dea498cced317f7556d3c8b050ab99c6f
enrique sanchez <enrique.sanchez@futurite.com>
2024-05-30 11:56:26 -0600

    Controller

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\GoogleAdsController;
 
-Route::get('/google-ads/authenticate', [GoogleAdsController::class, 'authenticate'])->name('google.ads.authenticate');
-Route::get('/google-ads/callback', [GoogleAdsController::class, 'authenticate'])->name('google.ads.callback');
-Route::get('/google-ads/campaigns', [GoogleAdsController::class, 'getCampaigns'])->name('google.ads.campaigns');
+Route::get('/google-ads/authenticate', [CampaignsController::class, 'authenticate'])->name('google.ads.authenticate');
+Route::get('/google-ads/callback', [CampaignsController::class, 'authenticate'])->name('google.ads.callback');
+Route::get('/google-ads/campaigns', [CampaignsController::class, 'getCampaigns'])->name('google.ads.campaigns');
 
 Route::get('/', function () {
     return view('welcome');
