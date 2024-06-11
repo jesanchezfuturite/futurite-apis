62c5a05187af96f8c4e5c7ba2ba03c85df7e76bc
enrique sanchez <enrique.sanchez@futurite.com>
2024-06-11 11:47:12 -0600

    Logs

--- app/Http/Controllers/GoogleAdsController.php
+++ app/Http/Controllers/GoogleAdsController.php
@@ -7,42 +7,47 @@ namespace App\Http\Controllers;
 use Google\Auth\OAuth2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Log;
 
 class GoogleAdsController extends Controller
 {
     public function handleGoogleAdsCallback(Request $request)
     {
-        $jsonKeyFilePath = storage_path('app/google-ads/credentials.json');
-        $credentials = json_decode(file_get_contents($jsonKeyFilePath), true);
+        try{
+            $jsonKeyFilePath = storage_path('app/google-ads/credentials.json');
+            $credentials = json_decode(file_get_contents($jsonKeyFilePath), true);
 
-        $webCredentials = $credentials['web'];
+            $webCredentials = $credentials['web'];
 
-        $oAuth2 = new OAuth2([
-            'clientId' => $webCredentials['client_id'],
-            'clientSecret' => $webCredentials['client_secret'],
-            'authorizationUri' => $webCredentials['auth_uri'],
-            'tokenCredentialUri' => $webCredentials['token_uri'],
-            'redirectUri' => $webCredentials['redirect_uris'][0],
-            'scope' => 'https://www.googleapis.com/auth/adwords',
-            'access_type' => 'offline', // Asegúrate de incluir access_type=offline
-            'approval_prompt' => 'force' // Forzar el prompt de aprobación para obtener el refresh token
-        ]);
+            $oAuth2 = new OAuth2([
+                'clientId' => $webCredentials['client_id'],
+                'clientSecret' => $webCredentials['client_secret'],
+                'authorizationUri' => $webCredentials['auth_uri'],
+                'tokenCredentialUri' => $webCredentials['token_uri'],
+                'redirectUri' => $webCredentials['redirect_uris'][0],
+                'scope' => 'https://www.googleapis.com/auth/adwords',
+                'access_type' => 'offline', // Asegúrate de incluir access_type=offline
+                'approval_prompt' => 'force' // Forzar el prompt de aprobación para obtener el refresh token
+            ]);
 
-        if ($request->has('code')) {
-            $oAuth2->setCode($request->input('code'));
-            $authToken = $oAuth2->fetchAuthToken();
+            if ($request->has('code')) {
+                $oAuth2->setCode($request->input('code'));
+                $authToken = $oAuth2->fetchAuthToken();
 
-            // Verificar y guardar el refresh token si está presente
-            if (!isset($authToken['refresh_token'])) {
-                return redirect()->route('home')->with('error', 'Failed to obtain refresh token. Please authorize the application again.');
+                // Verificar y guardar el refresh token si está presente
+                if (!isset($authToken['refresh_token'])) {
+                    return redirect()->route('home')->with('error', 'Failed to obtain refresh token. Please authorize the application again.');
+                }
+
+                // Guarda el token de acceso y el token de actualización
+                Storage::disk('local')->put('google-ads/google-ads-token.json', json_encode($authToken));
+
+                return redirect()->route('home')->with('success', 'Google Ads authenticated successfully!');
             }
-
-            // Guarda el token de acceso y el token de actualización
-            Storage::disk('local')->put('google-ads/google-ads-token.json', json_encode($authToken));
-
-            return redirect()->route('home')->with('success', 'Google Ads authenticated successfully!');
+            Log::info("[GoogleAdsController@handleGoogleAdsCallback] ERROR 'Failed to authenticate with Google Ads.'" );
+            return redirect()->route('home')->with('error', 'Failed to authenticate with Google Ads.');
+        }catch(\Exception $e){
+            Log::info("[GoogleAdsController@handleGoogleAdsCallback] ERROR " . json_encode($e) );
         }
-
-        return redirect()->route('home')->with('error', 'Failed to authenticate with Google Ads.');
     }
 }
