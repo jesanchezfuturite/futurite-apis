85d2f055ffb8161d83245aa675f3acacfceb1c7c
enrique sanchez <enrique.sanchez@futurite.com>
2024-06-11 15:03:33 -0600

    service

--- app/Console/Commands/ListGoogleAdsCustomers.php
+++ app/Console/Commands/ListGoogleAdsCustomers.php
@@ -1,11 +1,12 @@
 <?php
-/// app/Console/Commands/ListGoogleAdsCustomers.php
+// app/Console/Commands/ListGoogleAdsCustomers.php
 
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Google\Ads\GoogleAds\Lib\V16\GoogleAdsClient;
 use Google\Ads\GoogleAds\V16\Services\ListAccessibleCustomersRequest;
+use Google\Ads\GoogleAds\V16\Services\GoogleAdsServiceClient;
 
 class ListGoogleAdsCustomers extends Command
 {
@@ -30,11 +31,49 @@ class ListGoogleAdsCustomers extends Command
         // Llamar al método listAccessibleCustomers con el objeto request
         $response = $customerServiceClient->listAccessibleCustomers($request);
 
-        foreach ($response->getResourceNames() as $customer) {
-            $this->info("Customer: $customer");
+        // Obtener el servicio GoogleAdsServiceClient para realizar consultas
+        $googleAdsServiceClient = $this->googleAdsClient->getGoogleAdsServiceClient();
+
+        foreach ($response->getResourceNames() as $customerResourceName) {
+            $query = "
+                SELECT
+                    customer.id,
+                    customer.descriptive_name,
+                    customer.currency_code,
+                    customer.time_zone,
+                    customer.manager,
+                    customer.test_account,
+                    customer.resource_name,
+                    customer.applied_labels,
+                    customer_client.client_customer,
+                    customer_client.level,
+                    customer_client.hidden
+                FROM customer_client
+                WHERE customer_client.client_customer = '$customerResourceName'
+            ";
+
+            $searchResponse = $googleAdsServiceClient->search($customerResourceName, $query);
+
+            foreach ($searchResponse->getIterator() as $googleAdsRow) {
+                $customer = $googleAdsRow->getCustomer();
+                $customerClient = $googleAdsRow->getCustomerClient();
+
+                $this->info("Customer ID: " . $customer->getId());
+                $this->info("Descriptive Name: " . $customer->getDescriptiveName());
+                $this->info("Client Customer: " . $customerClient->getClientCustomer());
+                $this->info("Level: " . $customerClient->getLevel());
+                $this->info("Manager: " . ($customer->getManager() ? 'Yes' : 'No'));
+                $this->info("Currency Code: " . $customer->getCurrencyCode());
+                $this->info("Time Zone: " . $customer->getTimeZone());
+                $this->info("Hidden: " . ($customerClient->getHidden() ? 'Yes' : 'No'));
+                $this->info("Resource Name: " . $customer->getResourceName());
+                $this->info("Test Account: " . ($customer->getTestAccount() ? 'Yes' : 'No'));
+                $this->info("Applied Labels: " . implode(", ", iterator_to_array($customer->getAppliedLabels())));
+
+                $this->info("-------------------------------");
+            }
         }
 
         return 0;
-
     }
 }
