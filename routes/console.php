c30983ee09b1c5812f394414ef19df9fe8a6d6b7
enrique sanchez <enrique.sanchez@futurite.com>
2024-06-12 12:28:53 -0600

    Kernel

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,10 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('googleads:list-customers')->everyMinute();
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
