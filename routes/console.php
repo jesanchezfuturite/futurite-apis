a31f2ee596e56956d860d6b4e20393ad7cae5f7c
enrique sanchez <enrique.sanchez@futurite.com>
2024-07-12 13:52:45 -0600

    adjustments crontab

--- routes/console.php
+++ routes/console.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Artisan;
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('googleads:list-customers')->hourly();
-Schedule::command('googleads:list-campaigns')->hourlyAt(10);
-Schedule::command('googleads:update-indicators')->everyTwoHours(20);
+Schedule::command('googleads:list-customers')->everyFourHours(0);;
+Schedule::command('googleads:list-campaigns')->everyFourHours(10);;
+Schedule::command('googleads:update-indicators')->everyThreeHours(20);
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
